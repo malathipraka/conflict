3e6d4fa43f974ef04dfa4d85bf087030f791ba62
malathipraka <malathipriya.p@gmail.com>
2022-10-31 08:51:50 +0530

    sivagit

--- EmployeeDetails.java
+++ EmployeeDetails.java
@@ -10,11 +10,15 @@ public class EmployeeDetails {
 		private void empid() {
 		System.out.println("employee id :8072666170");
 		}
+		private void emplogin() {
+			System.out.println("employee login:80720");
+			}
 			public static void main(String[] args) {
 				EmployeeDetails E=new EmployeeDetails();
 				E.empmobile();
 				E.empname();
 				E.empid();
+				E.emplogin();
 			}
 
 	
